0110f87cc43723084735c343577ac7775a39e114
tayhooser <84413327+tayhooser@users.noreply.github.com>
2024-06-10 23:03:52 -0700

    animation tree added, running and falling

--- player.gd
+++ player.gd
@@ -4,11 +4,16 @@ extends Actor
 @export var ground_friction: float = .3
 @export var air_friction: float = .15
 
+#@onready var animation_tree : Animation 
+
 var feathers := 1 # should be set in some global var later
 var can_double_jump := (feathers > 2)
 var can_triple_jump := (feathers > 3)
 var direction
 var prev_direction
+
+func _ready():
+	$AnimationTree.active = true
 
 func _physics_process(delta):
 	# gravity
@@ -38,9 +43,11 @@ func _physics_process(delta):
 	if direction: # if pressing movement buttons
 		velocity.x = lerp(velocity.x, direction * max_speed, acceleration)
 		if direction != prev_direction and direction > 0:
-			$Marker2D.scale.x = 1
+			$Marker2D.scale.x = 1 # flip body
+			$HeadMarker.scale.x = 1 # flip head
 		elif direction != prev_direction and direction < 0:
 			$Marker2D.scale.x = -1
+			$HeadMarker.scale.x = -1
 	else:
 		if is_on_floor():
 			velocity.x = lerp(velocity.x, 0.0, ground_friction)
@@ -48,15 +55,36 @@ func _physics_process(delta):
 			velocity.x = lerp(velocity.x, 0.0, air_friction)
 	
 	# ANIMATION
+	update_animation_parameters()
+	'''
 	if is_on_floor():
 		if direction:
-			print("Walking animation...")
-			#$AnimationPlayer.play("walk")
+			#print("Walking animation...")
+			$AnimationPlayer.play("run")
 		else:
-			print("Idle animation...")
-			#$AnimationPlayer.play("idle")
+			#print("Idle animation...")
+			#$AnimationPlayer.reset()
+			$AnimationPlayer.play("idle")
 	else:
 		print("Falling animation...")
-		#$AnimationPlayer.play("falling")
+		$AnimationPlayer.play("falling")
+	'''
 
 	move_and_slide()
+
+func update_animation_parameters():
+	if is_on_floor():
+		$AnimationTree["parameters/conditions/not_falling"] = true
+		if direction:
+			$AnimationTree["parameters/conditions/idle"] = false
+			$AnimationTree["parameters/conditions/is_running"] = true
+		else:
+			$AnimationTree["parameters/conditions/idle"] = true
+			$AnimationTree["parameters/conditions/is_running"] = false
+	else:
+		$AnimationTree["parameters/conditions/not_falling"] = false
+		$AnimationTree.get("parameters/playback").travel("falling")
+		
+	if Input.is_action_just_pressed("bite"):
+		print("bite")
+		$AnimationTree.get("parameters/playback").travel("bite")
