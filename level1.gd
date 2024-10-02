37c91c6d2c278a205e13a367e5f8586301051ec3
tayhooser <84413327+tayhooser@users.noreply.github.com>
2024-10-02 16:07:28 -0700

    file cleanup

---
+++ level1.gd
@@ -0,0 +1,11 @@
+extends Level
+
+var cameraBottomLimit := 335
+var cameraRightLimit := 1744
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
