80379e7ea1e3f3d3a812715d1725996b81e5655a
tayhooser <84413327+tayhooser@users.noreply.github.com>
2024-10-04 18:59:25 -0700

    forgot to add new files

---
+++ hud.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	#var playerNode = get_parent().get_node("Player")
+	#$Label.text = "" + playerNode
+	pass
---
+++ notebook.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func resume():
+	get_tree().paused = false
+	
+func openNotebook():
+	get_tree()paused = true
